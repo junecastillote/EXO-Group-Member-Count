2715f0af6812cafe6ac7f6de38c51ddcbc332d08
June Castillote <june.castillote@gmail.com>
2025-08-14 11:19:56 +0800

    Update Get-EXOGroupMemberCount.ps1

--- Get-EXOGroupMemberCount.ps1
+++ Get-EXOGroupMemberCount.ps1
@@ -1,7 +1,7 @@
 # Get-EXOGroupMemberCount.ps1
 
 #Requires -Modules @{ ModuleName="ExchangeOnlineManagement"; ModuleVersion="3.7.0" }
-
+#Requires -Modules Microsoft.Graph.Authentication,Microsoft.Graph.Groups
 
 [CmdletBinding()]
 param (
@@ -9,10 +9,11 @@ param (
     $Identity,
 
     [Parameter()]
-    [switch]$ResolveOwner,
+    [string]$OutputCsv,
 
     [Parameter()]
-    [string]$OutputCsv,
+    [switch]
+    $Append,
 
     [Parameter()]
     [switch]$ReturnResult
@@ -22,10 +23,11 @@ begin {
     $scriptStart = [System.Diagnostics.Stopwatch]::StartNew()
 
     $results = [System.Collections.Generic.List[object]]::new()
-    $allOwnerIds = [System.Collections.Generic.HashSet[string]]::new()
 
-    $connected = Get-ConnectionInformation
-    if (-not $connected) {
+    $counter = 0
+
+    $exoConnected = Get-ConnectionInformation
+    if (-not $exoConnected) {
         Write-Error "Exchange Online PowerShell is not connected."
         if ($MyInvocation.InvocationName -like '*.ps1') {
             exit 1
@@ -36,37 +38,123 @@ begin {
         break
     }
 
+    $graphConnected = Get-MgContext
+    if (-not $graphConnected) {
+        Write-Error "Microsoft Graph PowerShell is not connected."
+        if ($MyInvocation.InvocationName -like '*.ps1') {
+            exit 1
+        }
+        else {
+            throw "Microsoft Graph PowerShell is not connected."
+        }
+        break
+    }
+
     if (-not $OutputCsv -and -not $ReturnResult) {
         Write-Verbose "Both -OutputCsv and -ReturnResult are not used. Enabling -ReturnResult by default."
         $ReturnResult = $true
     }
 
-    if ($OutputCsv -and (Test-Path $OutputCsv)) {
+    if ($OutputCsv -and (Test-Path $OutputCsv) -and -not $Append) {
         Write-Verbose "The output file [$OutputCsv)] already exists and will be overwritten."
         Remove-Item $OutputCsv -Force -Confirm:$false
     }
+
+    if ($OutputCsv -and (Test-Path $OutputCsv) -and $Append) {
+        Write-Verbose "The output file [$OutputCsv)] already exists and new results will be appended."
+        # Remove-Item $OutputCsv -Force -Confirm:$false
+    }
+
+    $acceptedTypes = @(
+        'Deserialized.Microsoft.Exchange.Data.Directory.Management.DynamicDistributionGroup',
+        'Deserialized.Microsoft.Exchange.Data.Directory.Management.DistributionGroup',
+        'Deserialized.Microsoft.Exchange.Data.Directory.Management.UnifiedGroupBase'
+    )
 }
 
 process {
+
     try {
-        $recipientObject = Get-Recipient -Identity $Identity -ErrorAction Stop
+        $counter++
+
+        # Get the object typename
+        $objTypeName = $Identity.psobject.typenames[0]
+
+        # Validate by typename
+        switch ($objTypeName) {
+
+            { $_ -in $acceptedTypes } {
+                $recipientObject = $Identity
+            }
+
+            'System.String' {
+                $recipientObject = Get-Recipient -Identity $Identity -ErrorAction Stop
+            }
+
+            default {
+                continue
+            }
+        }
+
+        $objTypeName = $recipientObject.psobject.TypeNames[0]
         $recipientId = $recipientObject.Identity
-        Write-Verbose "Processing [$($recipientObject.DisplayName)] | [$($recipientObject.RecipientTypeDetails)]"
+
+        Write-Verbose "Processing [#$($counter)] Type: [$($recipientObject.RecipientTypeDetails)] | Name: [$($recipientObject.DisplayName) / $($recipientObject.PrimarySmtpAddress)]"
 
         switch ($recipientObject.RecipientTypeDetails) {
             { $_ -in 'MailNonUniversalGroup', 'MailUniversalDistributionGroup', 'MailUniversalSecurityGroup' } {
-                $groupObject = Get-DistributionGroup -Identity $recipientId
-                $groupMemberCount = @(Get-DistributionGroupMember -Identity $recipientId -ResultSize Unlimited).Count
+
+                if ($objTypeName -notin $acceptedTypes) {
+                    $groupObject = Get-DistributionGroup -Identity $recipientId -IncludeManagedByWithDisplayNames
+                }
+
+                else {
+                    $groupObject = $recipientObject
+                }
+
+                if ($groupObject.ManagedBy -and !$groupObject.ManagedByWithDisplayName) {
+                    Write-Verbose "  -> Getting owner(s)"
+                    $groupObject = Get-DistributionGroup -Identity $recipientId -IncludeManagedByWithDisplayNames
+                }
+
+                $groupOwners = $groupObject.ManagedByWithDisplayName
+                $groupMemberCount = (Get-MgGroupMemberCount -GroupId $groupObject.ExternalDirectoryObjectId -ConsistencyLevel eventual)
                 $teamsEnabled = 'N/A'
             }
+
             { $_ -eq 'DynamicDistributionGroup' } {
-                $groupObject = Get-DynamicDistributionGroup -Identity $recipientId
-                $groupMemberCount = @(Get-Recipient -RecipientPreviewFilter $groupObject.RecipientFilter -ResultSize Unlimited).Count
+                if ($objTypeName -notin $acceptedTypes) {
+                    $groupObject = Get-DynamicDistributionGroup -Identity $recipientId -IncludeManagedByWithDisplayNames
+                }
+                else { $groupObject = $recipientObject }
+
+                if ($groupObject.ManagedBy -and !$groupObject.ManagedByWithDisplayName) {
+                    Write-Verbose "  -> Getting owner(s)"
+                    $groupObject = Get-DynamicDistributionGroup -Identity $recipientId -IncludeManagedByWithDisplayNames
+                }
+
+                $groupOwners = $groupObject.ManagedByWithDisplayName
+                $groupMemberCount = (Get-DynamicDistributionGroupMember -Identity $groupObject -ResultSize Unlimited | Measure-Object).Count
                 $teamsEnabled = 'N/A'
             }
+
             { $_ -eq 'GroupMailbox' } {
-                $groupObject = Get-UnifiedGroup -Identity $recipientId
+                if ($objTypeName -notin $acceptedTypes) {
+                    $groupObject = Get-UnifiedGroup -Identity $recipientId
+                }
+                else { $groupObject = $recipientObject }
+
                 $groupMemberCount = $groupObject.GroupMemberCount
+                $groupOwners = (Get-UnifiedGroupLinks -Identity $groupObject -LinkType Owners | ForEach-Object {
+                        $(
+                            if ($_.PrimarySmtpAddress) {
+                                "($($_.PrimarySmtpAddress), $($_.DisplayName))"
+                            }
+                            else {
+                                "($($_.WindowsLiveId), $($_.DisplayName))"
+                            }
+                        )
+                    })
                 $teamsEnabled = if ($groupObject.ResourceProvisioningOptions -contains 'Team') { 'Yes' } else { 'No' }
             }
             default {
@@ -74,19 +162,12 @@ process {
             }
         }
 
-        # Collect owners for later resolution if needed
-        if ($ResolveOwner -and $groupObject.ManagedBy) {
-            foreach ($ownerId in $groupObject.ManagedBy) {
-                $null = $allOwnerIds.Add([string]$ownerId)
-            }
-        }
-
         $results.Add([PSCustomObject]@{
-                GroupName    = $groupObject.Name
+                GroupName    = $groupObject.DisplayName
                 GroupEmail   = $groupObject.PrimarySmtpAddress
                 GroupType    = $groupObject.RecipientTypeDetails
                 TeamsEnabled = $teamsEnabled
-                Owners       = $groupObject.ManagedBy  # Temp placeholder
+                Owners       = $groupOwners -join ";"
                 MemberCount  = $groupMemberCount
             })
     }
@@ -97,39 +178,9 @@ process {
 }
 
 end {
-    $ownerResolutionStopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-    $ownerMap = @{}
-
-    # Resolve owners now in a single loop to avoid duplicates
-    if ($ResolveOwner -and $allOwnerIds.Count -gt 0) {
-        Write-Verbose "Performing owner name resolution..."
-        foreach ($ownerId in $allOwnerIds) {
-            try {
-                $recipient = Get-Recipient -Identity $ownerId -ErrorAction Stop
-                $ownerMap[$ownerId] = if ($recipient.WindowsLiveId) { $recipient.WindowsLiveId } else { $recipient.Id }
-            }
-            catch {
-                $ownerMap[$ownerId] = $ownerId
-            }
-        }
-    }
-    $ownerResolutionStopwatch.Stop()
-
-    # Replace placeholder Owners in results
-    foreach ($item in $results) {
-        if ($ResolveOwner -and $item.Owners) {
-            $item.Owners = ($item.Owners | ForEach-Object { $ownerMap[$_] }) -join ', '
-        }
-        elseif ($item.Owners) {
-            $item.Owners = $item.Owners -join ', '
-        }
-        else {
-            $item.Owners = ''
-        }
-    }
 
     if ($OutputCsv) {
-        $results | Export-Csv -NoTypeInformation -Path $OutputCsv
+        $results | Export-Csv -NoTypeInformation -Path $OutputCsv -Append:$Append
         Write-Verbose "Results saved to [$((Resolve-Path $OutputCsv).Path)]"
     }
 
@@ -139,6 +190,5 @@ end {
 
     $scriptStart.Stop()
 
-    Write-Verbose ("Owner resolution time: {0:N2} seconds" -f $ownerResolutionStopwatch.Elapsed.TotalSeconds)
     Write-Verbose ("Total script time: {0:N2} seconds" -f $scriptStart.Elapsed.TotalSeconds)
 }
